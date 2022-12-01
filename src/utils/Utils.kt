bc5cce542c8a583da82168e573856ce36153920b
Dayan Ruben <mail@dayanruben.com>
2022-12-01 14:53:56 -0300

    Simplified solution for day 1 of AOC 2022

--- src/utils/Utils.kt
+++ src/utils/Utils.kt
@@ -3,9 +3,19 @@ import java.math.BigInteger
 import java.security.MessageDigest
 
 /**
+ * Gets a File from the given resource folder (year) and file name (day).
+ */
+private fun fileFrom(name: String, year: String) = File("res/aoc$year", "$name.txt")
+
+/**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String, year: String) = File("res/aoc$year", "$name.txt").readLines()
+fun readInput(name: String, year: String) = fileFrom(name, year).readLines()
+
+/**
+ * Reads the entire content of the given input txt file as a String.
+ */
+fun readInputText(name: String, year: String) = fileFrom(name, year).readText()
 
 /**
  * Converts string to md5 hash.
