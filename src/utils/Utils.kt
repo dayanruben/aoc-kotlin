b7f6ae30fbea20a1b6b2e6002cac8bb5aa525103
Dayan Ruben <mail@dayanruben.com>
2023-11-30 23:16:28 -0300

    Use Path instead of File in read file utils

--- src/utils/Utils.kt
+++ src/utils/Utils.kt
@@ -1,28 +1,30 @@
-import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+import kotlin.io.path.readText
 
 /**
  * Gets a File from the given resource folder (year) and file name (day).
  */
-private fun fileFrom(name: String, year: String) = File("res/aoc$year", "$name.txt")
+private fun pathFrom(name: String, year: String) = Path("res/aoc$year/$name.txt")
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String, year: String) = fileFrom(name, year).readLines()
+fun readInput(name: String, year: String) = pathFrom(name, year).readLines()
 
 /**
  * Reads the entire content of the given input txt file as a String.
  */
-fun readInputText(name: String, year: String) = fileFrom(name, year).readText()
+fun readInputText(name: String, year: String) = pathFrom(name, year).readText()
 
 /**
  * Converts string to md5 hash.
  */
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
- .toString(16)
- .padStart(32, '0')
+        .toString(16)
+        .padStart(32, '0')
 
 /**
  * The cleaner shorthand for printing output.
