b5a1cf62832711765081ba1d6e5e48a6f0604224
Dayan Ruben <mail@dayanruben.com>
2023-11-30 22:59:51 -0300

    Added println extension function util and improved existing String.md5() implementation

--- src/utils/Utils.kt
+++ src/utils/Utils.kt
@@ -20,4 +20,11 @@ fun readInputText(name: String, year: String) = fileFrom(name, year).readText()
 /**
  * Converts string to md5 hash.
  */
-fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+ .toString(16)
+ .padStart(32, '0')
+
+/**
+ * The cleaner shorthand for printing output.
+ */
+fun Any?.println() = println(this)
