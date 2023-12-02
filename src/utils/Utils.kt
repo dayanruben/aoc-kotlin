ade01ca65ed32b9c9fc6e2cdd487492f951e4fdc
Dayan Ruben <mail@dayanruben.com>
2023-12-02 02:45:42 -0300

    Added utility function for check value with expected

--- src/utils/Utils.kt
+++ src/utils/Utils.kt
@@ -30,3 +30,14 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+
+/**
+ * Verifies whether the received value is equal to the expected value.
+ * Print values when are differents.
+ */
+fun <T> checkValue(value: T, expectedValue: T) {
+    check(value == expectedValue) {
+        "value=$value expected=$expectedValue"
+    }
+}
