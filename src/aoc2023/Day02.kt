8c95ab1e3c9f2a96ebb532840ba048f40fdd39d5
Dayan Ruben <mail@dayanruben.com>
2023-12-02 02:46:13 -0300

    Upload solutions for day 2 of AOC 2023

---
+++ src/aoc2023/Day02.kt
@@ -0,0 +1,65 @@
+package aoc2023
+
+import readInput
+import checkValue
+
+fun main() {
+    val (year, day) = "2023" to "Day02"
+
+    fun gameSets(game: String) =
+        game.split(";").map {
+            it.trim().split(",").map {
+                val (cubesStr, color) = it.trim().split(" ")
+                val cubes = cubesStr.toInt()
+                color to cubes
+            }
+        }
+
+    fun gamePossibility(game: String): Int {
+        val (gameId, gameInfo) = game.split(":")
+        val (_, id) = gameId.split(" ")
+
+        val sets = gameSets(gameInfo)
+        val possible = sets.all { set ->
+            set.all { (color, cubes) ->
+                when (color) {
+                    "red" -> cubes <= 12
+                    "green" -> cubes <= 13
+                    "blue" -> cubes <= 14
+                    else -> false
+                }
+            }
+        }
+
+        return if (possible) id.toInt() else 0
+    }
+
+    fun gamePower(game: String): Int {
+        val (_, gameInfo) = game.split(":")
+
+        val sets = gameSets(gameInfo)
+
+        val maxCubes = mutableMapOf("red" to 0, "green" to 0, "blue" to 0)
+        sets.forEach { set ->
+            set.forEach { (color, cubes) ->
+                maxCubes[color] = maxOf(maxCubes[color]!!, cubes)
+            }
+        }
+
+        return maxCubes.values.reduce { acc, i -> acc * i }
+    }
+
+    fun part1(input: List<String>) = input.sumOf { gamePossibility(it) }
+
+    fun part2(input: List<String>) = input.sumOf { gamePower(it) }
+
+    val testInput1 = readInput(name = "${day}_p1_test", year = year)
+    val testInput2 = readInput(name = "${day}_p2_test", year = year)
+    val input = readInput(name = day, year = year)
+
+    checkValue(part1(testInput1), 8)
+    println(part1(input))
+
+    checkValue(part2(testInput2), 2286)
+    println(part2(input))
+}
