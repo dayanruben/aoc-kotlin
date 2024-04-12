c0f6377eebf10e5f475f7d05fdd6da66f8851372
Dayan Ruben <mail@dayanruben.com>
2024-04-12 09:26:19 -0300

    Added amper dev version and extra maven repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,11 @@ pluginManagement {
         google()
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
+        maven("https://www.jetbrains.com/intellij-repository/releases")
+        maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
     }
 }
 
 plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.2.2")
+    id("org.jetbrains.amper.settings.plugin").version("0.2.3-dev-473")
 }
