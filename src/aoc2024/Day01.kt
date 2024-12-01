ba8ee766093909ce79121e1f660d81fc26da3992
Dayan Ruben <mail@dayanruben.com>
2024-12-01 08:48:15 -0300

    Upload solutions for day 1 of AOC 2024

--- src/aoc2024/Day01.kt
+++ src/aoc2024/Day01.kt
@@ -2,23 +2,44 @@ package aoc2024
 
 import checkValue
 import readInput
+import kotlin.math.abs
 
 fun main() {
     val (year, day) = "2024" to "Day01"
 
-    fun List<String>.test() = this.sumOf { it.toInt() }
+    fun part1(input: List<String>): Int {
+        val (left, right) = parseInput(input)
+        return left.zip(right).sumOf { (l, r) -> abs(l - r) }
+    }
 
-    fun part1(input: List<String>) = input.test()
+    fun part2(input: List<String>): Int {
+        val (left, right) = parseInput(input)
+        val counts = right.groupingBy { it }.eachCount()
+        return left.sumOf { it * (counts[it] ?: 0) }
+    }
 
-    fun part2(input: List<String>) = input.test()
-
-    val testInput1 = readInput(name = "${day}_p1_test", year = year)
-    val testInput2 = readInput(name = "${day}_p2_test", year = year)
+    val testInput = readInput(name = "${day}_test", year = year)
     val input = readInput(name = day, year = year)
 
-    checkValue(part1(testInput1), 0)
+    checkValue(part1(testInput), 11)
     println(part1(input))
 
-    checkValue(part2(testInput2), 0)
+    checkValue(part2(testInput), 31)
     println(part2(input))
+}
+
+private fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> {
+    val left = mutableListOf<Int>()
+    val right = mutableListOf<Int>()
+
+    input.forEach { line ->
+        val (l, r) = line.split("\\s+".toRegex()).map { it.toInt() }
+        left.add(l)
+        right.add(r)
+    }
+
+    left.sort()
+    right.sort()
+
+    return left to right
 }
